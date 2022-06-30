32ad5fe1ed22c7bf4cd20930714c9dbe2956688d
Lianne Dakanno <liannedakanno@gmail.com>
2022-06-29 21:53:40 -0500

    commit tree a0bf27becde4604548747d5871b359ef4c76c0a9

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+multiply;
 
-// Write your code here
+const random = Math.ceil(Math.random());
+random;
+
+const num3 = 14
+const num4 = 10
+const mod = num3 % num4;
+mod;
+
+const max = Math.max(5,10,15,20);
+max;
